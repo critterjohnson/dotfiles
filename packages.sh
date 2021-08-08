21c76090984e9310b43a6ff02e21530ba855da8c
Christopher Johnson <critterjohnson@pop-os.localdomain>
2021-08-08 13:09:39 -0400

    cmake

--- packages.sh
+++ packages.sh
@@ -9,4 +9,4 @@ sudo rm -rf /usr/local/go
 sudo tar -C /usr/local -xzf 'go1.16.7.linux-amd64.tar.gz'
 sudo rm -rf 'go1.16.7.linux-amd64.tar.gz'
 
-sudo apt-get install -y dkms libdrm-dev default-jre
+sudo apt-get install -y dkms libdrm-dev default-jre cmake
