00e759a153f427f7bb65f136183ecab0b4714690
Christopher Johnson <critterjohnson@pop-os.localdomain>
2021-08-09 09:09:05 -0400

    cmake

--- packages.sh
+++ packages.sh
@@ -4,14 +4,30 @@ set -e
 
 sudo apt-get update
 
-sudo wget 'https://golang.org/dl/go1.16.7.linux-amd64.tar.gz'
-sudo rm -rf /usr/local/go
-sudo tar -C /usr/local -xzf 'go1.16.7.linux-amd64.tar.gz'
-sudo rm -rf 'go1.16.7.linux-amd64.tar.gz'
+# go
+(
+    sudo wget 'https://golang.org/dl/go1.16.7.linux-amd64.tar.gz'
+    sudo rm -rf /usr/local/go
+    sudo tar -C /usr/local -xzf 'go1.16.7.linux-amd64.tar.gz'
+    sudo rm -rf 'go1.16.7.linux-amd64.tar.gz'
+)
 
-sudo apt-get install -y dkms libdrm-dev default-jre cmake nodejs npm zsh \
-    python3 python3-dev
+# regular packages
+sudo apt-get install -y dkms libdrm-dev default-jre nodejs npm zsh python3\
+    python3-dev build-essential git
 
-curl -LO 'https://github.com/BurntSushi/ripgrep/releases/download/12.1.1/ripgrep_12.1.1_amd64.deb'
-sudo dpkg -i 'ripgrep_12.1.1_amd64.deb'
-rm 'ripgrep_12.1.1_amd64.deb'
+# cmake from source
+(
+    git clone https://github.com/Kitware/CMake/
+    cd cmake
+    ./bootstrap
+    make
+    sudo make install
+)
+
+# ripgrep
+(
+    curl -LO 'https://github.com/BurntSushi/ripgrep/releases/download/12.1.1/ripgrep_12.1.1_amd64.deb'
+    sudo dpkg -i 'ripgrep_12.1.1_amd64.deb'
+    rm 'ripgrep_12.1.1_amd64.deb'
+)
