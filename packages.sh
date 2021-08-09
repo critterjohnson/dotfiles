b3879b2dc7388d3e00e06287b73ea34f2e92083e
Christopher Johnson <critterjohnson@pop-os.localdomain>
2021-08-09 09:01:54 -0400

    python

--- packages.sh
+++ packages.sh
@@ -9,7 +9,8 @@ sudo rm -rf /usr/local/go
 sudo tar -C /usr/local -xzf 'go1.16.7.linux-amd64.tar.gz'
 sudo rm -rf 'go1.16.7.linux-amd64.tar.gz'
 
-sudo apt-get install -y dkms libdrm-dev default-jre cmake nodejs npm zsh
+sudo apt-get install -y dkms libdrm-dev default-jre cmake nodejs npm zsh \
+    python3 python3-dev
 
 curl -LO 'https://github.com/BurntSushi/ripgrep/releases/download/12.1.1/ripgrep_12.1.1_amd64.deb'
 sudo dpkg -i 'ripgrep_12.1.1_amd64.deb'
