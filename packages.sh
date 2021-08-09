b93f6bfe2f526249cbcd6d051db15ecee13851eb
Christopher Johnson <critterjohnson@pop-os.localdomain>
2021-08-09 08:58:04 -0400

    ripgrep

--- packages.sh
+++ packages.sh
@@ -10,3 +10,6 @@ sudo tar -C /usr/local -xzf 'go1.16.7.linux-amd64.tar.gz'
 sudo rm -rf 'go1.16.7.linux-amd64.tar.gz'
 
 sudo apt-get install -y dkms libdrm-dev default-jre cmake nodejs npm zsh
+
+curl -LO 'https://github.com/BurntSushi/ripgrep/releases/download/12.1.1/ripgrep_12.1.1_amd64.deb'
+sudo dpkg -i 'ripgrep_12.1.1_amd64.deb'
